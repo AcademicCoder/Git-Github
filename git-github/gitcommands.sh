3afa613a42ce75f9f95d9fb651e6c762749740c3
mohamedabdiahmed <mohamedahmedabdiah@gmail.com>
2022-09-14 20:27:38 +0300

    waxaan ku soo daray casharo la xidhidha git ka

---
+++ git-github/gitcommands.sh
@@ -0,0 +1,28 @@
+ git command
+
+
+# habkan ayaa ah marka hore e aad isku xidhayso repositorygaagi online ka ahaa
+# git init : waxay inoo samaynaysa repostiry local ah 
+ 1. git init 
+# git add waxay inoogu daraysa files kii git localka 
+ 2. git add .
+#  git commit -m  waxaynu qexitaan u samaynaynaa files kan la dirayo
+ 3. git commit -m "qoraal qeexid ah"
+#  git branch ayaa ah mid lagu badalayo kii hore ee master ka ahaa mid main ah main branch waa mid heshis lagu yahay in la isticmaalo
+ 4. git branch -M main 
+#  git remote waxay inoo qabanaysa in ay isku ken xidho kii local ka ahaa iyo kii online ka ahaa
+ 5. git remote add origin link ga 
+#  git push waa in files kii localka yaalay loo rarayo reposiotry ga  online ka ah 
+ 6. git push -u origin  main 
+ 
+
+#  habkan aya ah in aad online ka intad kasoo qadato repositorygii aad dirayso uun anu kaaga baahnayn in aad ku dhex abuurto localkaaga 
+
+# kan ayaa ah in aad repositoryga aad kusoo dhex dajin karto localkii 
+1. git clone repository url kiisa 
+# git add ayaa ah inaad files kaagi localka ka rarayso lakinse maaha mid gaadhsinaya kii online ka ahaa 
+2. git add .
+# git commit ayaa ah inaad qeexitaan u samayso files ka ad dirayso 
+3. git commit -m "qeexitaan"
+# git push ayaa ah in aad u rarayso files kii online repository gaagii 
+4. git push 
---
+++ sharaxaad/hordhac.md
@@ -0,0 +1,22 @@
+# git-github
+
+# GIT
+
+git waa software kasoo aynu ku mamulno kooded keena 
+kaas oo local ah computer ka gudihiisa ayaad kaga maamuli kartaa wixii kood ah intii aanad ku dirin ka online ka ah e loo yaqaan github
+hadaba muxuu ku salaysan yahay git ku 
+
+waa awaamiir kala duwan kuwaas oo ah kuwo la isla yaqaan 
+aduunka oo dhamina isla ogyihiin 
+git ka waxa sameeyey linux ka kooxdiisa 
+
+# linux
+waa operitem system la isticmaalo kaas oo awoood xoogan ku leh 
+dhanka amaanka computerada waxana kuu kamid yahay ka loogu isticmaal badanyahay waxana isticmala shirkadaha wawayn si ay ugu ilaliyaan xogtooda dhex taala internetka iyo si gaar ahba 
+
+# git-commands 
+git commands waa awaamiir loo dajiyay in lagu maamulo 
+koodad kaaga hadii ay noqoto gudaha iyo online kaba
+hadaba git commands aynu isla eegno siday u kala horeyan 
+waana hab loo dajiyay in ay kala horeyn leeyihiin 
+ 
